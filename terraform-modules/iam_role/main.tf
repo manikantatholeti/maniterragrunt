10d0084c8486f92ac2860842f8994b24c1d1883b
manikantatoleti <manikanta.toleti@verinon.co.in>
2023-09-26 12:08:11 +0530

    added backend

--- terraform-modules/iam_role/main.tf
+++ terraform-modules/iam_role/main.tf
@@ -44,3 +44,13 @@ resource "aws_iam_role_policy" "fiserv_policy" {
 }
 EOF
 }
+remote_state {
+  backend = "s3"
+  config = {
+    bucket         = "test-bucket-terragrunt"
+    key            = "${path_relative_to_include()}/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "vtsl-automation-table-mgmt"
+  }
+}
