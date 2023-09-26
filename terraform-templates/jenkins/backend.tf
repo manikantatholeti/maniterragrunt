03ddf63d1900a74d1fb076383181370d0abb56a9
manikantatoleti <manikanta.toleti@verinon.co.in>
2023-09-26 11:49:26 +0530

    added backend

--- terraform-templates/jenkins/backend.tf
+++ terraform-templates/jenkins/backend.tf
@@ -2,7 +2,7 @@ remote_state {
   backend = "s3"
   config = {
     bucket         = "test-bucket-terragrunt"
-    key            = "terraform.tfstate"
+    key            = "${path_relative_to_include()}/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "vtsl-automation-table-mgmt"
